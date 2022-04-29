72fa028ac3c16c4158171959963eac0e1585a777
Ярослав Порываев <silencer86@yandex.ru>
2022-04-29 23:08:39 +0400

    Health check notification fix

--- LeagueActivityBot.Host/Startup.cs
+++ LeagueActivityBot.Host/Startup.cs
@@ -62,7 +62,7 @@ namespace LeagueActivityBot.Host
         {
             MigrationsRunner.ApplyMigrations(logger, serviceProvider, "LeagueActivityBot.Host").Wait();
             SummonersInitializer.Initialize(serviceProvider).Wait();
-            //StartupNotification.SendOnStartedUpNotification(serviceProvider, Manifest.ApplicationVersion).Wait();
+            StartupNotification.SendOnStartedUpNotification(serviceProvider, Manifest.ApplicationVersion).Wait();
             
             app.UseRouting();
 
