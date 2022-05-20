1c3157a48aa1b62953ed448f0cf330599f31692e
desoleilone <104645760+desoleilone@users.noreply.github.com>
2022-05-20 11:31:36 +0400

    rewrite rewrite
    Будет тогда лучше started to play

--- LeagueActivityBot/Notifications/OnSoloGameStarted/OnSoloGameStartedMessageBuilder.cs
+++ LeagueActivityBot/Notifications/OnSoloGameStarted/OnSoloGameStartedMessageBuilder.cs
@@ -7,7 +7,7 @@ namespace LeagueActivityBot.Notifications.OnSoloGameStarted
     {
         public string Build(OnSoloGameStartedNotification notification)
         {
-            var sb = new StringBuilder($"{notification.Summoner.GetName()} totally doesn't give a fuck about his friends and started playing solo {QueueType.GetQueueTypeById(notification.QueueTypeId)}");
+            var sb = new StringBuilder($"{notification.Summoner.GetName()} totally doesn't give a fuck about his friends and started to play solo {QueueType.GetQueueTypeById(notification.QueueTypeId)}");
 
             return sb.ToString();
         } 
