29c20886c42a6da93f9be5fac786b16ba3c6494d
Ярослав Порываев <silencer86@yandex.ru>
2023-06-19 21:51:24 +0400

    api url fix

--- ClientApp/src/constants.ts
+++ ClientApp/src/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://expwr.ru/server";
+export const BASE_URL = "https://api.expwr.ru";
 export const AVATAR_URL =
   "https://ddragon.leagueoflegends.com/cdn/13.12.1/img/champion/";
